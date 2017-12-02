676a50315e4414b8113632fe8ad8e3ebb82df120
Omer <Omertorrents@gmail.com>
2017-12-02 19:03:44 +0200

    mla handlers

--- handlers/roomsHandler.js
+++ handlers/roomsHandler.js
@@ -4,7 +4,7 @@ var dbAdapter = require('../dbAdapter.js');
 var db = dbAdapter.db;
 
 router.get('/',function (req, res) {
-    let q = db.all("SELECT r_name, r_id, r_slots, r_start, r_end from rooms").then(function (row) {
+    var q = db.all("SELECT r_name, r_id, r_slots, r_start, r_end from rooms").then(function (row) {
         res.send(JSON.stringify(row));
     });
     q.catch(function (err) {

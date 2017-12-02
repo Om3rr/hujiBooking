8dfaface8e5b0a013fd6d7ccadf0833165cce2ab
Omer <Omertorrents@gmail.com>
2017-12-02 19:22:58 +0200

    mla handlers

--- common/mailer.js
+++ common/mailer.js
@@ -3,6 +3,7 @@ var nodemailer = require('nodemailer');
 function makeid() {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    
     for (var i = 0; i < 12; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
 
